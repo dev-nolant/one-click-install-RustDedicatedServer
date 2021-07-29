9438e7a45052a1cf96949bab27f3500fdf0f7f23
Nolan Taft <killer123yo@gmail.com>
2021-07-29 15:05:29 -0600

    Update step-two.bat

--- step-two.bat
+++ step-two.bat
@@ -9,9 +9,12 @@ md %wrkdir%\steamapps\common\rust_dedicated\temp_zip
     bitsadmin /transfer "start_installer" "https://raw.githubusercontent.com/dev-nolant/one-click-install-RustDedicatedServer/main/start.bat" "%cd%\start.bat"
     cls
     echo "Finished - Starting Server"
-    call start.bat
     del "zipjsalt.bat" /f /q
     del "zipjs.bat" /f /q
+    del "server-install.bat" /f /q
+    start start.bat
+    exit
+
 :zipjs
     zipjs.bat unzip -source "%wrkdir%\umod.zip" -destination "%wrkdir%\steamapps\common\rust_dedicated\temp_zip" -keep yes -force yes
     GOTO finish
