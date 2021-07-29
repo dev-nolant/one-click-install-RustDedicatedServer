5d538b04f5c75763a8fd321d4379074abc3e1e59
Nolan Taft <killer123yo@gmail.com>
2021-07-29 12:49:38 -0600

    Update zipjs_alt.bat

--- zipjs_alt.bat
+++ zipjs_alt.bat
@@ -7,7 +7,7 @@ IF "%1"=="t" GOTO Third
 SHIFT
 :First
 echo "Transfering Files to Unzipped Folder 1/2"
-call zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -force yes -keep yes 
+zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -keep yes -force no
 echo "moving on"
 exit
 :Second
@@ -16,5 +16,5 @@ start steamcmd.exe +login anonymous +app_update 258550 valdiate +quit
 exit
 :Third
 echo "Init uMod and Installing"
-call zipjs.bat unzip -source "%wrkdir%\umod.zip" -destination %wrkdir%\steamapps\common\rust_dedicated -force yes -keep yes
+zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -keep yes -force no
 exit
