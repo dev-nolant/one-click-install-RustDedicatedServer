a1d6f46726241c0fb9abad3686a69d296d758c5d
Nolan Taft <killer123yo@gmail.com>
2021-07-29 16:35:51 -0600

    Update server-install.bat

--- server-install.bat
+++ server-install.bat
@@ -5,7 +5,7 @@ bitsadmin /transfer "steamcmd"  https://steamcdn-a.akamaihd.net/client/installer
 bitsadmin /transfer "zipjsbat" https://raw.githubusercontent.com/npocmaka/batch.scripts/master/hybrids/jscript/zipjs.bat "%cd%\zipjs.bat"
 bitsadmin /transfer "zipjsbat" https://raw.githubusercontent.com/dev-nolant/one-click-install-RustDedicatedServer/main/zipjs_alt.bat "%cd%\zipjsalt.bat"
 bitsadmin /transfer "zipjsbat" https://raw.githubusercontent.com/dev-nolant/one-click-install-RustDedicatedServer/main/step-two.bat "%cd%\step-two.bat"
-bitsadmin /transfer "uMod" https://umod.org/games/rust/download?tag=public "%wrkdir%\umod.zip"
+bitsadmin /transfer "uMod" https://umod.org/games/rust/download?tag=public "%cd%\umod.zip"
 
 call zipjsalt.bat f
 del "%cd%\steamcmd.zip"
