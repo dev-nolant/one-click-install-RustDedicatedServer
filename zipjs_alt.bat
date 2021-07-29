8f1c9e5adbd73f8e6c5dd0cfdbfe703a623d0f50
Nolan Taft <killer123yo@gmail.com>
2021-07-29 11:48:44 -0600

    Update zipjs_alt.bat

--- zipjs_alt.bat
+++ zipjs_alt.bat
@@ -1,2 +1,15 @@
+@ECHO OFF
 set wrkdir="%cd%\Server-Files"
+IF "%1"=="f" GOTO First
+IF "%1"=="s" GOTO Second
+IF "%1"=="t" GOTO Third
+SHIFT
+:First
 zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -force yes
+exit
+:Second
+steamcmd.exe +login anonymous +app_update 258550 valdiate +quit
+exit
+:Third
+zipjs.bat unzip -source "%wrkdir%\umod.zip" -destination %wrkdir%\steamapps\common\rust_dedicated -force yes
+exit
