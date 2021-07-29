24d157f4f8fb69efc56442c0fce4503fd76fb170
Nolan Taft <killer123yo@gmail.com>
2021-07-29 12:18:07 -0600

    Update zipjs_alt.bat

--- zipjs_alt.bat
+++ zipjs_alt.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 set wrkdir="%cd%\Server-Files"
-cd %wrkdir%
+
 IF "%1"=="f" GOTO First
 IF "%1"=="s" GOTO Second
 IF "%1"=="t" GOTO Third
 SHIFT
 :First
-call zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -keep yes -force yes
+call zipjs.bat unzip -source "steamcmd.zip" -destination %wrkdir% -keep yes -force yes
 exit
 :Second
 start steamcmd.exe +login anonymous +app_update 258550 valdiate +quit
