b9f2170f0b35a6d17e5852465437e202b598adc4
Nolan Taft <killer123yo@gmail.com>
2021-07-29 14:03:44 -0600

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,3 @@
+:start
+start Server-Files\steamapps\common\rust_dedicated\RustDedicated.exe -batchmode +server.port 28015 +server.level "Procedural Map" +server.seed 50500 +server.worldsize 2500 +server.maxplayers 10 +server.hostname "You did it!"
+goto start
