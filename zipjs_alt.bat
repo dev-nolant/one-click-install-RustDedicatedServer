02cc23c9a607820ab3de9c485ab7526e876a9745
Nolan Taft <killer123yo@gmail.com>
2021-07-29 12:06:25 -0600

    Update zipjs_alt.bat

--- zipjs_alt.bat
+++ zipjs_alt.bat
@@ -5,7 +5,7 @@ IF "%1"=="s" GOTO Second
 IF "%1"=="t" GOTO Third
 SHIFT
 :First
-zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -keep yes -force yes
+call zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -keep yes -force yes
 exit
 :Second
 cd %wrkdir%
@@ -13,5 +13,5 @@ start steamcmd.exe +login anonymous +app_update 258550 valdiate +quit
 exit
 :Third
 cd %wrkdir%
-zipjs.bat unzip -source "%wrkdir%\umod.zip" -destination %wrkdir%\steamapps\common\rust_dedicated -keep yes -force yes
+call zipjs.bat unzip -source "%wrkdir%\umod.zip" -destination %wrkdir%\steamapps\common\rust_dedicated -keep yes -force yes
 exit
