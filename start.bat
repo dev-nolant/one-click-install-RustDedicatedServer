b88da47d184e466eb11bebd6f62dab5d1a8ac6ad
Nolan Taft <killer123yo@gmail.com>
2021-07-29 15:32:48 -0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
 del "step-two.bat"/f /q
 cls
+cd Server-Files\steamapps\common\rust_dedicated\
 :start
-start Server-Files\steamapps\common\rust_dedicated\RustDedicated.exe -batchmode +server.port 28015 +server.level "Procedural Map" +server.seed 50500 +server.worldsize 2500 +server.maxplayers 10 +server.hostname "You did it!"
+RustDedicated.exe -batchmode +server.port 28015 +server.level "Procedural Map" +server.seed 50500 +server.worldsize 2500 +server.maxplayers 10 +server.hostname "You did it!"
 goto start
