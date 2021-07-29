6921eb36a896abd9d5e4556fdf262c5ea0ba846f
Nolan Taft <killer123yo@gmail.com>
2021-07-29 11:54:45 -0600

    Update zipjs_alt.bat

--- zipjs_alt.bat
+++ zipjs_alt.bat
@@ -8,8 +8,10 @@ SHIFT
 zipjs.bat unzip -source "%cd%\steamcmd.zip" -destination %wrkdir% -keep yes -force yes
 exit
 :Second
+cd %wrkdir%
 steamcmd.exe +login anonymous +app_update 258550 valdiate +quit
 exit
 :Third
+cd %wrkdir%
 zipjs.bat unzip -source "%wrkdir%\umod.zip" -destination %wrkdir%\steamapps\common\rust_dedicated -keep yes -force yes
 exit
