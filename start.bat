eac798fc5a8364b2770bfe3241aed98d01e197b8
Nolan Taft <killer123yo@gmail.com>
2021-07-29 18:00:57 -0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
 @echo off
 del "step-two.bat"/f /q
 cls
+echo "Deleted umod.zip"
+del "umod.zip" /f /q
 cd Server-Files\steamapps\common\rust_dedicated\
 :start
 RustDedicated.exe -batchmode +server.port 28015 +server.level "Procedural Map" +server.seed 50500 +server.worldsize 2500 +server.maxplayers 10 +server.hostname "You did it!"
