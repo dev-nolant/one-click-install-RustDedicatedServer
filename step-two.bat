b67dbdc600bb3e4200fbb7d7d23c9119501fe87b
Nolan Taft <killer123yo@gmail.com>
2021-07-29 18:19:05 -0600

    Update step-two.bat

--- step-two.bat
+++ step-two.bat
@@ -1,22 +1,15 @@
 @echo off
 echo "Init uMod and Installing"
 set wrkdir="%cd%\Server-Files"
-md %wrkdir%\steamapps\common\rust_dedicated\temp_zip
+md %wrkdir%\steamapps\common\rust_dedicated\temp_zip  
+call zipjsalt.bat t
+move "Server-Files\steamapps\common\rust_dedicated\temp_zip\RustDedicated_Data\Managed\*dll**.*" "Server-Files\steamapps\common\rust_dedicated\RustDedicated_Data\Managed"
 
-:finish      
-    move "Server-Files\steamapps\common\rust_dedicated\temp_zip\RustDedicated_Data\Managed\*dll**.*" "Server-Files\steamapps\common\rust_dedicated\RustDedicated_Data\Managed"
-    
-    bitsadmin /transfer "start_installer" "https://raw.githubusercontent.com/dev-nolant/one-click-install-RustDedicatedServer/main/start.bat" "%cd%\start.bat"
-    cls
-    echo "Finished - Starting Server"
-    del "zipjsalt.bat" /f /q
-    del "zipjs.bat" /f /q
-    del "server-install.bat" /f /q
-    start start.bat
-    exit
-
-
-:zipjs
-    zipjs.bat unzip -source "%cd%\umod.zip" -destination "%wrkdir%\steamapps\common\rust_dedicated\temp_zip" -keep yes -force yes
-    goto finish
-GOTO zipjs
+bitsadmin /transfer "start_installer" "https://raw.githubusercontent.com/dev-nolant/one-click-install-RustDedicatedServer/main/start.bat" "%cd%\start.bat"
+cls
+echo "Finished - Starting Server"
+del "zipjsalt.bat" /f /q
+del "zipjs.bat" /f /q
+del "server-install.bat" /f /q
+start start.bat
+exit
