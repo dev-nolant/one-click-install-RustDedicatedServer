1558f273d2747a462a75c9497330d7af6cb8d2a8
Nolan Taft <killer123yo@gmail.com>
2021-07-29 18:19:14 -0600

    Update zipjs_alt.bat

--- zipjs_alt.bat
+++ zipjs_alt.bat
@@ -3,6 +3,7 @@ set wrkdir="%cd%\Server-Files"
 
 IF "%1"=="f" GOTO First
 IF "%1"=="s" GOTO Second
+IF "%1"=="t" GOTO temp-keep
 SHIFT
 :First
 echo "Transfering Files to Unzipped Folder 1/2"
@@ -12,5 +13,8 @@ exit
 :Second
 echo "Installing Rust-Dedicated Server Files"
 start Server-Files\steamcmd.exe +login anonymous +app_update 258550 valdiate +quit
+exit
+:temp-keep 
+zipjs.bat unzip -source "%cd%\umod.zip" -destination "%wrkdir%\steamapps\common\rust_dedicated\temp_zip" -keep yes -force yes
 exit
 
